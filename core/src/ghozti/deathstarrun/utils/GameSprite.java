47244169d4fdc306f57e3e14132fe1d3aac402aa
ybald <ybalderas1005@gmail.com>
2021-12-28 22:07:04 -0500

    - worked on GameSprite.java

--- core/src/ghozti/deathstarrun/utils/GameSprite.java
+++ core/src/ghozti/deathstarrun/utils/GameSprite.java
@@ -1,6 +1,7 @@
 package ghozti.deathstarrun.utils;
 
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class GameSprite {
     //my own modification of the sprite class
@@ -11,6 +12,29 @@ public class GameSprite {
     private float hitboxOffsetx, hitboxOffsetY;
     private boolean debugMode;
 
+    public GameSprite(Sprite sprite, float[] positionArray, float width, float height, float scale, float unscaledWidth, float unscaledHeight, float hitboxOffsetx, float hitboxOffsetY, boolean debugMode, TextureRegion region){
+        this.sprite = sprite;
+        this.sprite.setRegion(region);
 
+        this.positionArray = positionArray;
+        this.sprite.setPosition(this.positionArray[0],this.positionArray[1]);
+
+        this.width = width;
+        this.height = height;
+
+        this.scale = scale;
+        this.sprite.setScale(this.scale);
+
+        this.unscaledWidth = unscaledWidth;
+        this.sprite.setRegionWidth((int)unscaledWidth);
+        this.unscaledHeight = unscaledHeight;
+        this.sprite.setRegionHeight((int)unscaledHeight);
+
+        this.hitboxOffsetx = hitboxOffsetx;
+        this.hitboxOffsetY = hitboxOffsetY;
+        this.debugMode = debugMode;
+
+        this.sprite.setOrigin(0,0);
+    }
 
 }
