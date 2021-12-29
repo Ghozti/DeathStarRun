40d67ead9cd66b60008485608b20db30b490c365
ybald <ybalderas1005@gmail.com>
2021-12-29 18:46:13 -0500

    - worked on GameSprite.java

--- core/src/ghozti/deathstarrun/utils/GameSprite.java
+++ core/src/ghozti/deathstarrun/utils/GameSprite.java
@@ -1,18 +1,24 @@
 package ghozti.deathstarrun.utils;
 
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Rectangle;
 
-public class GameSprite {
+public abstract class GameSprite {
     //my own modification of the sprite class
 
     private Sprite sprite;
     private float[] positionArray;//index 0 -> x axis, index 1 -> y axis
     private float width,height,scale,unscaledWidth,unscaledHeight;
     private float hitboxOffsetx, hitboxOffsetY;
+    private Rectangle rectangle;
     private boolean debugMode;
 
-    public GameSprite(Sprite sprite, float[] positionArray, float width, float height, float scale, float unscaledWidth, float unscaledHeight, float hitboxOffsetx, float hitboxOffsetY, boolean debugMode, TextureRegion region){
+    //constant textures
+    TextureRegion hitboxTexture;
+
+    public GameSprite(Sprite sprite, float[] positionArray, float width, float height, float scale, float unscaledWidth, float unscaledHeight, float hitboxOffsetx, float hitboxOffsetY, Rectangle rectangle, boolean debugMode, TextureRegion region){
         this.sprite = sprite;
         this.sprite.setRegion(region);
 
@@ -32,9 +38,90 @@ public class GameSprite {
 
         this.hitboxOffsetx = hitboxOffsetx;
         this.hitboxOffsetY = hitboxOffsetY;
+
+        this.rectangle = rectangle;
+        this.rectangle.x = positionArray[0];
+        this.rectangle.y = positionArray[1];
+        this.rectangle.width = width;
+        this.rectangle.height = height;
+        this.rectangle.x += hitboxOffsetx;
+        this.rectangle.y += hitboxOffsetY;
+
         this.debugMode = debugMode;
 
         this.sprite.setOrigin(0,0);
     }
 
+    public Sprite getSprite() {
+        return sprite;
+    }
+
+    public float[] getPositionArray() {
+        return positionArray;
+    }
+
+    public float getWidth() {
+        return width;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public float getScale() {
+        return scale;
+    }
+
+    public float getUnscaledWidth() {
+        return unscaledWidth;
+    }
+
+    public float getUnscaledHeight() {
+        return unscaledHeight;
+    }
+
+    public float getHitboxOffsetx() {
+        return hitboxOffsetx;
+    }
+
+    public float getHitboxOffsetY() {
+        return hitboxOffsetY;
+    }
+
+    public Rectangle getRectangle() {
+        return rectangle;
+    }
+
+    public boolean isDebugMode() {
+        return debugMode;
+    }
+
+    public void setSpriteOrigin(float x, float y){
+        sprite.setOrigin(x,y);
+    }
+
+    public void updateSpritePosition(float xChange, float yChange){
+        positionArray[0] += xChange;
+        positionArray[1] += yChange;
+        rectangle.x += xChange;
+        rectangle.y += yChange;
+        sprite.setPosition(positionArray[0], positionArray[1]);
+    }
+
+    //DRAWING STUFF
+    public abstract void draw(Batch batch);
+
+    //this method will be used to update the sprite for example user input
+    public abstract void update();
+
+    public void drawSprite(Batch batch) {
+        sprite.draw(batch);
+    }
+
+    //will draw the hitbox if the game is in debug mode
+    public void drawHitBox(Batch batch) {
+        if(debugMode) {
+            batch.draw(hitboxTexture, rectangle.x, rectangle.y, width, height);
+        }
+    }
 }
