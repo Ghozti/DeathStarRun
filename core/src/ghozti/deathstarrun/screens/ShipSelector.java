1d4fcf68845ff44a08fb5e1798ffc920d5a8afbe
Ghozti <ybalderas1005@gmail.com>
2022-01-11 11:19:36 -0500

    - worked on ShipSelector

--- core/src/ghozti/deathstarrun/screens/ShipSelector.java
+++ core/src/ghozti/deathstarrun/screens/ShipSelector.java
@@ -1,20 +1,30 @@
 package ghozti.deathstarrun.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class ShipSelector implements Screen {
 
     SpriteBatch batch;
-
+    Texture backGround;
+    Texture ship;
+    String teamSelected;
 
     public ShipSelector(String teamSelected){
-
+        batch = new SpriteBatch();
+        backGround = new Texture(Gdx.files.internal("core/assets/death-star-runAssets/teamSelectorBG.png"));
+        this.teamSelected = teamSelected;
     }
 
     @Override
     public void show() {
+
+    }
+
+    public void update(){
 
     }
 
