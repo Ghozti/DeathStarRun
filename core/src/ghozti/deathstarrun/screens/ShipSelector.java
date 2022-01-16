332482e4895f61854ddebf16f168da13d16a51fb
Ghozti <ybalderas1005@gmail.com>
2022-01-15 21:20:22 -0500

    - allowed for ship modification

--- core/src/ghozti/deathstarrun/screens/ShipSelector.java
+++ core/src/ghozti/deathstarrun/screens/ShipSelector.java
@@ -2,6 +2,7 @@ package ghozti.deathstarrun.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -18,7 +19,7 @@ public class ShipSelector implements Screen {
 
     String teamSelected;
     SpriteBatch batch;
-    Texture backGround, leftArrow, rigthArrow;
+    Texture backGround, leftArrow, rigthArrow, currentShip;
     TextureRegion hitboxTexture;
     com.badlogic.gdx.math.Rectangle rightArrowHitbox, leftArrowHitbox, shipHitbox, mouseHitbox;
     ArrayList<Texture> shipList;
@@ -30,12 +31,11 @@ public class ShipSelector implements Screen {
         leftArrow = new Texture(Gdx.files.internal("core/assets/death-star-run-startAssets/leftArrow1.png"));
         rigthArrow = new Texture(Gdx.files.internal("core/assets/death-star-run-startAssets/rightArrow1.png"));
         hitboxTexture = Atlas.getHitbox();
-        leftArrowHitbox = new com.badlogic.gdx.math.Rectangle(120,430,220,220);
-        rightArrowHitbox =  new com.badlogic.gdx.math.Rectangle(1580,430,220,220);
+        leftArrowHitbox = new Rectangle(120,430,220,220);
+        rightArrowHitbox =  new Rectangle(1580,430,220,220);
+        shipHitbox = new Rectangle(560,140,800,800);
         mouseHitbox = new Rectangle(Gdx.input.getX(),Math.abs(Gdx.input.getY() - (int) Constants.Screen.HEIGHT),10,10);
-        if (teamSelected.equals(Constants.Teams.REBELS)){
-
-        }
+        setShipList(teamSelected);
     }
 
     @Override
@@ -55,8 +55,10 @@ public class ShipSelector implements Screen {
         batch.begin();
         batch.draw(rigthArrow,rightArrowHitbox.x,rightArrowHitbox.y,rightArrowHitbox.width,rightArrowHitbox.height);
         batch.draw(leftArrow,leftArrowHitbox.x,leftArrowHitbox.y,leftArrowHitbox.width,leftArrowHitbox.height);
+        batch.draw(currentShip,shipHitbox.x,shipHitbox.y,shipHitbox.width,shipHitbox.height);
         batch.draw(hitboxTexture,mouseHitbox.x,mouseHitbox.y,mouseHitbox.width,mouseHitbox.height);
         batch.draw(hitboxTexture,rightArrowHitbox.x,rightArrowHitbox.y,rightArrowHitbox.width,rightArrowHitbox.height);
+        batch.draw(hitboxTexture,shipHitbox.x,shipHitbox.y,shipHitbox.width,shipHitbox.height);
         batch.draw(hitboxTexture,leftArrowHitbox.x,leftArrowHitbox.y,leftArrowHitbox.width,leftArrowHitbox.height);
         batch.end();
     }
@@ -86,11 +88,23 @@ public class ShipSelector implements Screen {
         batch.dispose();
     }
 
+    int currentIndex = 0;
+    boolean coolDown;
+
     private void handleArrowInput(){
         if (mouseHitbox.overlaps(leftArrowHitbox)){
             leftArrow = new Texture("core/assets/death-star-run-startAssets/leftArrow2.png");
             if (Gdx.input.isButtonPressed(Input.Keys.LEFT)){
                 leftArrow = new Texture("core/assets/death-star-run-startAssets/leftArrow3.png");
+                if (!coolDown){
+                    coolDown = true;
+                    if (currentIndex > 0){
+                        currentIndex--;
+                    }
+                    currentShip = shipList.get(currentIndex);
+                }
+            }else {
+                coolDown = false;
             }
         }else {
             leftArrow = new Texture("core/assets/death-star-run-startAssets/leftArrow1.png");
@@ -100,6 +114,15 @@ public class ShipSelector implements Screen {
             rigthArrow = new Texture("core/assets/death-star-run-startAssets/rightArrow2.png");
             if (Gdx.input.isButtonPressed(Input.Keys.LEFT)){
                 rigthArrow = new Texture("core/assets/death-star-run-startAssets/rightArrow3.png");
+                if (!coolDown){
+                    coolDown = true;
+                    if (currentIndex < shipList.size()-1){
+                        currentIndex++;
+                    }
+                    currentShip = shipList.get(currentIndex);
+                }
+            }else {
+                coolDown = false;
             }
         }else {
             rigthArrow = new Texture("core/assets/death-star-run-startAssets/rightArrow1.png");
@@ -112,8 +135,16 @@ public class ShipSelector implements Screen {
     }
 
     private void setShipList(String team){
+        shipList = new ArrayList<>();
         if (team.equals(Constants.Teams.REBELS)){
-            shipList.add(new Texture("core/assets/death-star-run-startAssets/rightArrow1.png"));
+            shipList.add(new Texture("core/assets/death-star-run-startAssets/xwing.png"));
+            shipList.add(new Texture("core/assets/death-star-run-startAssets/xwingGold.png"));
+            shipList.add(new Texture("core/assets/death-star-run-startAssets/xwingGreen.png"));
+            shipList.add(new Texture("core/assets/death-star-run-startAssets/xwingOrange.png"));
+            shipList.add(new Texture("core/assets/death-star-run-startAssets/xwingPurple.png"));
+            shipList.add(new Texture("core/assets/death-star-run-startAssets/xwingRed.png"));
+
+            currentShip = shipList.get(0);
         }else if(team.equals(Constants.Teams.IMPERIALS)){
 
         }
