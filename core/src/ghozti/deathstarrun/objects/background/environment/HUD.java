89f2b538798ec0588a18569816661d78a565cecc
Ghozti <ybalderas1005@gmail.com>
2022-01-31 20:40:38 -0500

    - worked on HUD

--- core/src/ghozti/deathstarrun/objects/background/environment/HUD.java
+++ core/src/ghozti/deathstarrun/objects/background/environment/HUD.java
@@ -1,4 +1,17 @@
 package ghozti.deathstarrun.objects.background.environment;
 
+import com.badlogic.gdx.graphics.g2d.Batch;
+import ghozti.deathstarrun.utils.Font;
+
 public class HUD {
+
+    Font font;
+
+    public HUD(){
+        font = new Font(32);
+    }
+
+    public void drawHud(Batch batch, float score){
+        font.draw(batch,"Score: " + score, 32,1000,0,false);
+    }
 }
