87c89923a5a22887ed80991c6e9e86b02986a1b3
Ghozti <ybalderas1005@gmail.com>
2022-02-06 22:04:44 -0500

    - made InteractableBackGroundItem an abstract class

--- core/src/ghozti/deathstarrun/objects/background/environment/Pole.java
+++ core/src/ghozti/deathstarrun/objects/background/environment/Pole.java
@@ -4,24 +4,5 @@ import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.math.Rectangle;
 import ghozti.deathstarrun.objects.background.utils.InteractableBackGroundItem;
 
-public class Pole implements InteractableBackGroundItem {
-    @Override
-    public void onTargetCollision(Rectangle target) {
-
-    }
-
-    @Override
-    public void update() {
-
-    }
-
-    @Override
-    public void draw(Batch batch) {
-
-    }
-
-    @Override
-    public void drawHitbox(Batch batch) {
-
-    }
+public class Pole {
 }
--- core/src/ghozti/deathstarrun/objects/background/utils/InteractableBackGroundItem.java
+++ core/src/ghozti/deathstarrun/objects/background/utils/InteractableBackGroundItem.java
@@ -1,15 +1,88 @@
 package ghozti.deathstarrun.objects.background.utils;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.math.Rectangle;
 
-public interface InteractableBackGroundItem {
+public abstract class InteractableBackGroundItem {
 
-    public void onTargetCollision(Rectangle target);
+    Texture texture;
+    Rectangle hitbox;
+    float[] posArray;
+    float speed;
+    float width, height;
+    float damageGiven;
 
-    public void update();
+    public InteractableBackGroundItem(){
 
-    public void draw(Batch batch);
+    }
 
-    public void drawHitbox(Batch batch);
+    //***************************SETTERS********************************************************************************
+    public void setTexture(Texture texture) {
+        this.texture = texture;
+    }
+
+    public void setHitbox(Rectangle hitbox) {
+        this.hitbox = hitbox;
+    }
+
+    public void setPosArray(float[] posArray) {
+        this.posArray = posArray;
+    }
+
+    public void setSpeed(float speed) {
+        this.speed = speed;
+    }
+
+    public void setWidth(float width) {
+        this.width = width;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public void setDamageGiven(float damageGiven) {
+        this.damageGiven = damageGiven;
+    }
+
+    //***************************GETTERS********************************************************************************
+
+    public Texture getTexture() {
+        return texture;
+    }
+
+    public Rectangle getHitbox() {
+        return hitbox;
+    }
+
+    public float[] getPosArray() {
+        return posArray;
+    }
+
+    public float getSpeed() {
+        return speed;
+    }
+
+    public float getWidth() {
+        return width;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public float getDamageGiven() {
+        return damageGiven;
+    }
+
+    /******************************************************************************************************************/
+
+    public abstract void onTargetCollision(Rectangle target);
+
+    public abstract void update();
+
+    public abstract void draw(Batch batch);
+
+    public abstract void drawHitbox(Batch batch);
 }
