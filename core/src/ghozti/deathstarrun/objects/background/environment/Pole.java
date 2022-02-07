b6fd20162619e4bb27142969a8b0ee003b2963fa
Ghozti <ybalderas1005@gmail.com>
2022-02-06 22:07:06 -0500

    - added BackGroundObjectRenderer

--- core/src/ghozti/deathstarrun/objects/background/environment/Pole.java
+++ core/src/ghozti/deathstarrun/objects/background/environment/Pole.java
@@ -4,5 +4,29 @@ import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.math.Rectangle;
 import ghozti.deathstarrun.objects.background.utils.InteractableBackGroundItem;
 
-public class Pole {
+public class Pole extends InteractableBackGroundItem{
+
+    public Pole(float x, float y){
+        setPosArray(new float[] {x, y});
+    }
+
+    @Override
+    public void onTargetCollision(Rectangle target) {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void draw(Batch batch) {
+
+    }
+
+    @Override
+    public void drawHitbox(Batch batch) {
+
+    }
 }
