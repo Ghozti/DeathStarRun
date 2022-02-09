5823ce7b06b5b2cce4d4e4b8d531aa0d817f0b17
Ghozti <ybalderas1005@gmail.com>
2022-02-08 22:11:35 -0500

    - worked on stuff

--- core/src/ghozti/deathstarrun/objects/background/environment/BackGroundObjectGroupRenderer.java
+++ core/src/ghozti/deathstarrun/objects/background/environment/BackGroundObjectGroupRenderer.java
@@ -4,6 +4,11 @@ import ghozti.deathstarrun.objects.background.utils.InteractableBackGroundItem;
 
 public class BackGroundObjectGroupRenderer {
 
+    /*
+        RULES FOR OBJECTS:
+
+     */
+
     private InteractableBackGroundItem getNext(){
         //will calculate next object to render
         return null;
