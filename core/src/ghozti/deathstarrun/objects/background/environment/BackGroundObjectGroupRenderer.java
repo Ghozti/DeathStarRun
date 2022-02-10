6fded56ae483152630414def9f07144872bb2cbc
Ghozti <62896836+Ghozti@users.noreply.github.com>
2022-02-09 21:16:56 -0500

    - worked on stuff

--- core/src/ghozti/deathstarrun/objects/background/environment/BackGroundObjectGroupRenderer.java
+++ core/src/ghozti/deathstarrun/objects/background/environment/BackGroundObjectGroupRenderer.java
@@ -7,7 +7,18 @@ public class BackGroundObjectGroupRenderer {
     /*
         RULES FOR OBJECTS:
 
+       - no more than 3 objects at once
+
+       - only big objects can deal 100% damage
+
+       - object has to start to be rendered above the screen borders and will become invalid after it's 60 px below the bottom border
      */
+
+    //final float rightSide =
+
+    private double[] getRandomizedCoordinates(){
+        return null;
+    }
 
     private InteractableBackGroundItem getNext(){
         //will calculate next object to render
